b07ecc7ccec23688e43c5b282ecbe66cd28f57c0
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2023-01-17 21:27:25 -0500

    Add `doc.go`
    Signed-off-by: Kent 'picat' Gruber <kent.picat.gruber@gmail.com>

---
+++ pkg/csaf/doc.go
@@ -0,0 +1,4 @@
+// Package csaf provides a library for Common Security Advisory Framework Version 2.0 (CSAF) documents.
+//
+// https://docs.oasis-open.org/csaf/csaf/v2.0/os/csaf-v2.0-os.html
+package csaf
