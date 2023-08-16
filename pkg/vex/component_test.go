27166fce674775b8334103ac2ab4735a4b623a47
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-08-16 13:31:56 -0600

    Add identifier matching tests
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ pkg/vex/component_test.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2023 The OpenVEX Authors
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package vex
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestComponentMatches(t *testing.T) {
+	for testCase, tc := range map[string]struct {
+		identifier string
+		component  *Component
+		mustMatch  bool
+	}{
+		"iri": {
+			"https://example.com/document.spdx.json#node",
+			&Component{ID: "https://example.com/document.spdx.json#node"},
+			true,
+		},
+		"misc identifier": {
+			"madeup-2023-12345",
+			&Component{
+				Identifiers: map[IdentifierType]string{"customIdentifier": "madeup-2023-12345"},
+			},
+			true,
+		},
+		"wrong misc identifier": {
+			"madeup-2023-12345",
+			&Component{
+				Identifiers: map[IdentifierType]string{"customIdentifier": "another-string"},
+			},
+			false,
+		},
+		"same purl": {
+			"pkg:apk/wolfi/curl@8.1.2-r0?arch=x86_64",
+			&Component{
+				Identifiers: map[IdentifierType]string{PURL: "pkg:apk/wolfi/curl@8.1.2-r0?arch=x86_64"},
+			},
+			true,
+		},
+		"globing purl": {
+			"pkg:oci/curl@sha256:47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c",
+			&Component{
+				Identifiers: map[IdentifierType]string{PURL: "pkg:oci/curl"},
+			},
+			true,
+		},
+		"globing purl (inverse)": {
+			"pkg:oci/curl",
+			&Component{
+				Identifiers: map[IdentifierType]string{
+					PURL: "pkg:oci/curl@sha256:47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c",
+				},
+			},
+			false,
+		},
+		"hash": {
+			"77d86e9752cb933569dfa1f693ee4338e65b28b4",
+			&Component{
+				Hashes: map[Algorithm]Hash{
+					SHA1: "77d86e9752cb933569dfa1f693ee4338e65b28b4",
+				},
+			},
+			true,
+		},
+		"wrong hash": {
+			"77d86e9752cb933569dfa1f693ee4338e65b28b4",
+			&Component{
+				Hashes: map[Algorithm]Hash{
+					SHA1: "b5cc41d90d7ccc195c4a24ceb32656942c9854ea",
+				},
+			},
+			false,
+		},
+	} {
+		require.Equal(t, tc.mustMatch, tc.component.Matches(tc.identifier), fmt.Sprintf("failed: %s", testCase))
+	}
+}
---
+++ pkg/vex/product_test.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2023 The OpenVEX Authors
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package vex
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestProductMatches(t *testing.T) {
+	for testCase, tc := range map[string]struct {
+		sut          *Product
+		product      string
+		subcomponent string
+		mustMach     bool
+	}{
+		"identifier only": {
+			sut: &Product{
+				Component: Component{ID: "pkg:apk/alpine/libcrypto3@3.0.8-r3"},
+			},
+			product:      "pkg:apk/alpine/libcrypto3@3.0.8-r3",
+			subcomponent: "",
+			mustMach:     true,
+		},
+		"purl only": {
+			sut: &Product{
+				Component: Component{Identifiers: map[IdentifierType]string{
+					PURL: "pkg:apk/alpine/libcrypto3@3.0.8-r3",
+				}},
+			},
+			product:      "pkg:apk/alpine/libcrypto3@3.0.8-r3",
+			subcomponent: "",
+			mustMach:     true,
+		},
+		"generic purl only": {
+			sut: &Product{
+				Component: Component{Identifiers: map[IdentifierType]string{
+					PURL: "pkg:apk/alpine/libcrypto3",
+				}},
+			},
+			product:      "pkg:apk/alpine/libcrypto3@3.0.8-r3",
+			subcomponent: "",
+			mustMach:     true,
+		},
+		"identifier and components in doc and statement": {
+			sut: &Product{
+				Component: Component{ID: "pkg:oci/alpine@sha256%3A124c7d2707904eea7431fffe91522a01e5a861a624ee31d03372cc1d138a3126"},
+				Subcomponents: []Subcomponent{
+					{
+						Component{ID: "pkg:apk/alpine/libcrypto3@3.0.8-r3"},
+					},
+				},
+			},
+			product:      "pkg:oci/alpine@sha256%3A124c7d2707904eea7431fffe91522a01e5a861a624ee31d03372cc1d138a3126",
+			subcomponent: "pkg:apk/alpine/libcrypto3@3.0.8-r3",
+			mustMach:     true,
+		},
+		"identifier and no components in query": {
+			sut: &Product{
+				Component: Component{ID: "pkg:oci/alpine@sha256%3A124c7d2707904eea7431fffe91522a01e5a861a624ee31d03372cc1d138a3126"},
+				Subcomponents: []Subcomponent{
+					{
+						Component{ID: "pkg:apk/alpine/libcrypto3@3.0.8-r3"},
+					},
+				},
+			},
+			product:      "pkg:oci/alpine@sha256%3A124c7d2707904eea7431fffe91522a01e5a861a624ee31d03372cc1d138a3126",
+			subcomponent: "",
+			mustMach:     true,
+		},
+		"identifier and no components in document": {
+			sut: &Product{
+				Component:     Component{ID: "pkg:oci/alpine@sha256%3A124c7d2707904eea7431fffe91522a01e5a861a624ee31d03372cc1d138a3126"},
+				Subcomponents: []Subcomponent{},
+			},
+			product:      "pkg:oci/alpine@sha256%3A124c7d2707904eea7431fffe91522a01e5a861a624ee31d03372cc1d138a3126",
+			subcomponent: "pkg:apk/alpine/libcrypto3@3.0.8-r3",
+			mustMach:     true,
+		},
+		"identifier + multicomponent doc": {
+			sut: &Product{
+				Component: Component{ID: "pkg:oci/alpine@sha256%3A124c7d2707904eea7431fffe91522a01e5a861a624ee31d03372cc1d138a3126"},
+				Subcomponents: []Subcomponent{
+					{Component{ID: "pkg:apk/alpine/libcrypto3@3.0.8-r3"}},
+					{Component{ID: "pkg:apk/alpine/libssl@3.0.8-r3"}},
+				},
+			},
+			product:      "pkg:oci/alpine@sha256%3A124c7d2707904eea7431fffe91522a01e5a861a624ee31d03372cc1d138a3126",
+			subcomponent: "pkg:apk/alpine/libssl@3.0.8-r3",
+			mustMach:     true,
+		},
+	} {
+		require.Equal(t, tc.mustMach, tc.sut.Matches(tc.product, tc.subcomponent), fmt.Sprintf("failed: %s", testCase))
+	}
+}
